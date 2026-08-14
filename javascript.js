152f399b2a142a5306925b735d4f3bbfd9e253e5
aveskovac-trace <290512200+aveskovac-trace@users.noreply.github.com>
2026-08-14 11:51:07 +0200

    Add Variables and Operators assignment

--- javascript.js
+++ javascript.js
@@ -10,3 +10,22 @@ console.log(age); // outputs 11 to the console
 age = 54;
 
 console.log(age); // what will be output now?
+
+console.log(23 + 97)
+console.log(6 + 2 + 5 + 9 + 22 + 1)
+console.log((4 + 6 + 9) / 77)
+
+let a = 10
+console.log(a)
+
+let b = 7* a
+console.log(b)
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max; 
+console.log(percentage)
+
+age = 28
+const ageInMonths = age * 12
+console.log(ageInMonths)
